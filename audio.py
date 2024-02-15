8ced561f7861a4edc81d3b46ef4cf39893167012
Amol <59217989+amolkambar@users.noreply.github.com>
2024-02-15 11:23:13 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,18 +1,20 @@
-import yt_dlp
-
-from pytube import Playlist
 import os
+import time
+import yt_dlp
+from pytube import Playlist
+import threading
+import concurrent.futures
+
 
 folder_name=input("Input Folder name for the files: ")
-
 os.mkdir(folder_name)
 
 playlist_url = input("Paste Playlist URL: ")
-
 ids = Playlist(playlist_url)
 
-URLS=[]
+start_time = time.time()
 
+URLS=[]
 for id in ids:
     URLS.append(id)
 print(f"Downloading {len(URLS)} files")
@@ -22,16 +24,13 @@ ydl_opts = {
     'no_warnings':True,
     'output':r'test',
     'format': 'm4a/bestaudio/best',
-    'postprocessors': [{  # Extract audio using ffmpeg
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'm4a',
-    }],
-    'outtmpl':folder_name + '/%(title)s.%(ext)s'
+    'metadatafromtitle':True,
+    'writethumbnail':True,
+    'outtmpl':folder_name + '/%(title)s.%(ext)s',
+    'postprocessors':[{'key': 'FFmpegMetadata',
+                        'add_metadata': True,},
+                      {'key': 'EmbedThumbnail',}]
 }
-
-
-import threading
-import concurrent.futures
 
 def download_audio(file):
     links = [file]
@@ -40,3 +39,4 @@ def download_audio(file):
 with concurrent.futures.ThreadPoolExecutor() as executor:
         executor.map(download_audio, URLS)
 
+print(f'Download completed in {int(time.time()-start_time)} Seconds')
